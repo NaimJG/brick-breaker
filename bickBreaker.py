f892c9047c9d801ec256e29b3803ce276a0e8640
Naim Cambe <naimjgcambe@gmail.com>
2024-02-13 00:53:01 -0300

    Añadido movimiento y límites a la bolita

--- bickBreaker.py
+++ bickBreaker.py
@@ -5,6 +5,7 @@ from pygame.sprite import AbstractGroup
 
 ANCHO = 640
 ALTO = 480
+color_azul = (0 , 0, 64) # Color azul para el fondo
 
 class Bolita(pygame.sprite.Sprite):
     def __init__(self):
@@ -16,20 +17,43 @@ class Bolita(pygame.sprite.Sprite):
         # Posición inicial centrada en pantalla
         self.rect.centerx = ANCHO/2
         self.rect.centery = ALTO/2
+        # Establecer velocidad inicial
+        self.speed = [3, 3]
 
-# Inicializand pantalla
+    def update(self):
+        # Evitar que salga por debajo
+        if self.rect.bottom >= ALTO or self.rect.top <= 0:
+            self.speed[1] = -self.speed[1]
+        elif self.rect.right >= ANCHO or self.rect.left <= 0:
+            self.speed[0] = -self.speed[0]
+
+        # Mover en base a posición actual y velocidad
+        self.rect.move_ip(self.speed)
+
+# Inicializando pantalla
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 
 # Configurar el título de la pantalla
 pygame.display.set_caption('Brick Breaker')
 
+# Crear el reloj
+reloj = pygame.time.Clock()
+
 bolita = Bolita()
 
 while True:
+    # Establecer FPS
+    reloj.tick(60)
+
     # Revisar todos los eventos
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
+
+    # Actualizar posición de la bolita
+    bolita.update()
+
+    pantalla.fill(color_azul)
 
     # Dibujar bolita en pantalla
     pantalla.blit(bolita.image, bolita.rect)
