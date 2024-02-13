756d44a323718589b4a25056c2129c998bfa9946
Naim Cambe <naimjgcambe@gmail.com>
2024-02-13 01:08:03 -0300

    Añadida la paleta del jugador con su movimiento y límites

--- bickBreaker.py
+++ bickBreaker.py
@@ -30,6 +30,30 @@ class Bolita(pygame.sprite.Sprite):
         # Mover en base a posición actual y velocidad
         self.rect.move_ip(self.speed)
 
+class Paleta(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        # Cargar imagen
+        self.image = pygame.image.load('imagenes/paleta.png')
+        # Obtener rectángulo de la imagen
+        self.rect = self.image.get_rect()
+        # Posición inicial centrada en pantalla en X
+        self.rect.midbottom = (ANCHO/2, ALTO - 20)
+        # Establecer velocidad inicial
+        self.speed = [0, 0]
+
+    def update(self, evento):
+        # Buscar si se presionó la flecha izquierda
+        if evento.key == pygame.K_LEFT and self.rect.left > 0:
+            self.speed = [-5, 0]
+        # Si se presionó flecha derecha
+        elif evento.key == pygame.K_RIGHT and self.rect.right < ANCHO:
+            self.speed = [5, 0]
+        else:
+            self.speed = [0, 0]
+        # Mover en base a posición actual y velocidad
+        self.rect.move_ip(self.speed)
+
 # Inicializando pantalla
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 
@@ -39,7 +63,11 @@ pygame.display.set_caption('Brick Breaker')
 # Crear el reloj
 reloj = pygame.time.Clock()
 
+# Ajustar repetición de evento de tecla presionada.
+pygame.key.set_repeat(20)
+
 bolita = Bolita()
+jugador = Paleta()
 
 while True:
     # Establecer FPS
@@ -49,6 +77,9 @@ while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             sys.exit()
+        # Buscar eventos del teclado
+        elif evento.type == pygame.KEYDOWN:
+            jugador.update(evento)
 
     # Actualizar posición de la bolita
     bolita.update()
@@ -58,8 +89,9 @@ while True:
     # Dibujar bolita en pantalla
     pantalla.blit(bolita.image, bolita.rect)
 
-    # Actualizar los elementos en pantalla
+    # Dibujar jugador en pantalla
+    pantalla.blit(jugador.image, jugador.rect)
     
-
+    # Actualizar los elementos en pantalla
     pygame.display.flip()
 
